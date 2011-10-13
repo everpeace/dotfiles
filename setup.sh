c34ca2d1345d7a9ad40ba68e918cc7dd7c69d268
Shingo Omura <everpeace@gmail.com>
2011-10-13 10:15:38 +0900

    setup.sh works fine if dotfiles can be cloned to any path.

--- setup.sh
+++ setup.sh
@@ -1,8 +1,26 @@
-ln -s $HOME/dotfiles/.bash_profile $HOME/.bash_profile
-ln -s $HOME/dotfiles/.gitignore $HOME/.gitignore
-ln -s $HOME/dotfiles/.vim $HOME/.vim
-ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc
-ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc
+#/bin/bash
+
+function make_link()
+{
+  if [ $# -eq 1 ];then
+    _filename=$1
+    echo "ln -s $FROM/$_filename $TO/$_filename"
+    ln -s $FROM/$_filename $TO/$_filename
+  fi
+}
+
+# スクリプトのある場所
+FROM=$(dirname $0)
+FROM=`cd $FROM;pwd`
+
+# リンクを張る場所
+TO=$HOME
+
+make_link .bash_profile
+make_link .gitignore
+make_link .vim
+make_link .vimrc
+make_link .zshrc
 
 IFS=:
 GIT=false
@@ -14,8 +32,8 @@ then
   echo "git found!"
   # git config --global user.name "Shingo Omura"
   # git config --global user.email "everpeace@gmail.com"
-  # git config --global core.excludesfile "$HOME/.gitignore"
-  ln -s $HOME/dotfiles/.gitconfig $HOME/.gitconfig
+  # git config --global core.excludesfile "$TO/.gitignore"
+  make_link .gitconfig
 else echo "no git"
 fi
 
