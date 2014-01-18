7da91bdc2f0f4ddc4220d3ccd21b7ee4796b93d9
Shingo Omura <everpeace@gmail.com>
2014-01-17 18:33:12 -0800

    add israel time clock

--- bin/myworldclocks.sh
+++ bin/myworldclocks.sh
@@ -7,11 +7,12 @@ BTN=`env TZ=US/Eastern date +"%m/%d(%a)  %I:%M %p %Z"`
 LDN=`env TZ=Europe/London date +"%m/%d(%a)  %I:%M %p %Z"`
 SHN=`env TZ=Asia/Shanghai date +"%m/%d(%a)  %I:%M %p %Z"`
 PHL=`env TZ=Asia/Manila date +"%m/%d(%a)  %I:%M %p %Z"`
+TEL=`env TZ=Israel date +"%m/%d(%a)  %I:%M %p %Z"`
 
 echo "SanFran  $SFO"
 echo "Boston   $BTN"
+echo "TelAviv  $TEL"
 echo "London   $LDN"
 echo "Shanhai  $SHN"
 echo "Manila   $PHL"
 echo "Tokyo    $TKY"
-
