73ec338c7d8219d595aec2cfa27c904562e484df
Shingo Omura <everpeace@gmail.com>
2015-04-02 02:06:55 -0700

    added here to myworldclocks.sh

--- bin/myworldclocks.sh
+++ bin/myworldclocks.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # http://en.wikipedia.com/wiki/List_of_tz_database_time_zones
 
+HERE=`date +"%m/%d(%a)  %I:%M %p %Z"`
 TKY=`env TZ=Asia/Tokyo date +"%m/%d(%a)  %I:%M %p %Z" `
 SFO=`env TZ=US/Pacific date +"%m/%d(%a)  %I:%M %p %Z"`
 BTN=`env TZ=US/Eastern date +"%m/%d(%a)  %I:%M %p %Z"`
@@ -9,6 +10,7 @@ SHN=`env TZ=Asia/Shanghai date +"%m/%d(%a)  %I:%M %p %Z"`
 PHL=`env TZ=Asia/Manila date +"%m/%d(%a)  %I:%M %p %Z"`
 TEL=`env TZ=Israel date +"%m/%d(%a)  %I:%M %p %Z"`
 
+echo "Here     $HERE"
 echo "SanFran  $SFO"
 echo "Boston   $BTN"
 echo "TelAviv  $TEL"
