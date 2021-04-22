c9a2182363f49e7f98b678bc3e7ca34d24471193
Shingo Omura <everpeace@gmail.com>
2021-04-23 00:04:18 +0900

    set gitub token to use github search

--- .zshrc.d/ghq.sh
+++ .zshrc.d/ghq.sh
@@ -3,7 +3,7 @@ export GHQ_ROOT=$HOME/src
 # gh(e)i: ghs + peco + ghq import utilities
 function ghi(){
   [ "$#" -eq 0 ] && echo "Usage : gpi QUERY" && return 1
-  ghs "$@" | peco | awk '{print $1}' | ghq import -p
+  ghs -t $(cat ~/.vault/github.com.token | grep token= | sed -e 's/.*token=//') "$@" | peco | awk '{print $1}' | ghq get -p
 }
 
 function ghqcd(){
