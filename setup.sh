9ca7afdf7f2b6336b2f04c88e824672145ba3073
Shingo Omura <everpeace@gmail.com>
2021-04-24 04:35:53 +0000

    refine setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
 LN="ln -hfs"  # mac
-if ! ln -hfs /tmp/dummy >/dev/null 2>&1; then 
-  LN="ln -fs" # linux
+if ! ln -hfs /tmp/dummy >/dev/null 2>&1; then
+  LN="ln -fsT" # linux
 fi
 
 function make_link()
@@ -34,6 +34,7 @@ make_link .vimrc
 make_link .zshrc
 make_link .zshrc.d
 make_link peco .config/peco
+mkdir -p $HOME/.config
 make_link powerline .config/powerline
 make_link starship.toml .config/starship.toml
 make_link .Rprofile
@@ -43,20 +44,21 @@ make_link .mac-provisioning
 make_link .tigrc
 make_link .octaverc
 make_link .ocamlinit
+make_link .gitconfig
+make_link .gitignore_global
+make_link .gitconfig.d
+make_link vault .vault
 
-IFS=:
-GIT=false
-for d in $PATH
-do test -x "$d/git" && GIT=true
-done
-if $GIT
-then
-  echo "git found!"
-  # git config --global user.name "Shingo Omura"
-  # git config --global user.email "everpeace@gmail.com"
-  # git config --global core.excludesfile "$TO/.gitignore"
-  make_link .gitconfig
-  make_link .gitignore_global
-  make_link .gitconfig.d
-else echo "no git"
-fi
+# IFS=:
+# GIT=false
+# for d in $PATH
+# do test -x "$d/git" && GIT=true
+# done
+# if $GIT
+# then
+#   echo "git found!"
+#   # git config --global user.name "Shingo Omura"
+#   # git config --global user.email "everpeace@gmail.com"
+#   # git config --global core.excludesfile "$TO/.gitignore"
+# else echo "no git"
+# fi
