418665196a8b06b7992f3e64d2e71ddea4b6c93e
Shingo Omura <everpeace@gmail.com>
2021-10-19 23:35:45 +0900

    simple name to jump git repo working dirs

--- .zshrc.d/ghq.sh
+++ .zshrc.d/ghq.sh
@@ -6,7 +6,7 @@ function ghi(){
   ghs -t $(cat ~/.vault/github.com.token | grep token= | sed -e 's/.*token=//') "$@" | peco | awk '{print $1}' | ghq get -p
 }
 
-function ghqcd(){
+function g(){
   # [ "$#" -eq 0 ] && echo "Usage : ghqcd QUERY" && return 1
   local full_path
   full_path=$(ghq list -p -e "$(ghq list "$@" | peco | awk '{print $1}')")
