86e82ef082f5aa2f0079b806efbdc2c17fc5205d
Shingo Omura <everpeace@gmail.com>
2022-09-18 12:40:06 +0900

    add secretive

---
+++ .zshrc.d/secretive.sh
@@ -0,0 +1,3 @@
+if [ -e "$HOME/Library/Containers/com.maxgoedjen.Secretive.SecretAgent/Data/socket.ssh" ]; then
+    export SSH_AUTH_SOCK=/Users/everpeace/Library/Containers/com.maxgoedjen.Secretive.SecretAgent/Data/socket.ssh
+fi
